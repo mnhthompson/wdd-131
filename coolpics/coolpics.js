109458aea147b2f4c5c3a7c9c280628b0346a6ab
Mnhthompson <mnhthompson@gmail.com>
2025-01-20 11:07:01 -0700

    ?????

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -13,8 +13,8 @@ function handleResize() {
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 function viewerTemplate(pic, alt) {
-    return `<div class="viewer" id="image-viewer">
-        <button class="close-viewer" id="close-btn">X</button>
+    return `<div class="viewer" >
+        <button class="close-viewer">X</button>
         <img src="${pic}" alt="${alt}">
     </div>`;
   }
