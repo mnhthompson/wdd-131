73a8bb6f4f4951c456b423b25799d251c766789a
Mnhthompson <mnhthompson@gmail.com>
2025-01-20 12:29:31 -0700

    ignore

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,16 +1,16 @@
 
+
 document.addEventListener('DOMContentLoaded', () => {
-function handleResize() {
-    const menu = document.querySelector(".menu");
-    if (window.innerWidth > 1000) {
-      menu.classList.remove("hide");
-    } else {
-      menu.classList.add("hide");
-    }
-  }
+
+
+//    function handleResize() {
+ //   const menu = document.querySelector(".menu");
+ //   if (window.innerWidth > 1000) {menu.classList.remove("hide");} 
+ // else {menu.classList.add("hide");}
+ // }
   
-  handleResize();
-  window.addEventListener("resize", handleResize);
+ // handleResize();
+ // window.addEventListener("resize", handleResize);
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 function viewerTemplate(pic, alt) {
@@ -59,4 +59,3 @@ function closeViewer() {
     }}
 
 });
-//grape soda is delioushious so tasty
